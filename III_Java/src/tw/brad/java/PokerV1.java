8fcc068ed78f5124bf0ef0b50d18d0474c6f0fdb
USER <USER@L04.r1804.tciii.org.tw>
2021-03-02 14:14:57 +0800

    03021414

---
+++ III_Java/src/tw/brad/java/PokerV1.java
@@ -0,0 +1,29 @@
+package tw.brad.java;
+
+public class PokerV1 {
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		int[] poker = new int[52];	// poker[0] ~ poker[51]
+		for (int i=0; i<poker.length; i++) {
+			int rand = (int)(Math.random()*52);	// 0 - 51;
+			// ÀË¬d¾÷¨î
+			boolean isRepeat = false;
+			for (int j=0; j<i; j++) {
+				if (poker[j] == rand) {
+					isRepeat = true;
+					break;
+				}
+			}
+			if (!isRepeat) {
+				poker[i] = rand;
+				System.out.println(poker[i]);
+			}else {
+				i--;
+			}
+		}
+		System.out.println("------");
+		System.out.println(System.currentTimeMillis() - start);
+	}
+
+}
