98e69fb6c1eb84f97a1f89053f4d27ee43e76e50
USER <USER@L04.r1804.tciii.org.tw>
2021-03-02 16:29:50 +0800

    03021629

--- III_Java/src/tw/brad/java/PokerV3.java
+++ III_Java/src/tw/brad/java/PokerV3.java
@@ -1,5 +1,7 @@
 package tw.brad.java;
 
+import java.util.Arrays;
+
 public class PokerV3 {
 
 	public static void main(String[] args) {
@@ -34,9 +36,13 @@ public class PokerV3 {
 		}
 		
 		// 攤牌
+		String[] suits = {"黑桃","紅心","方塊","梅花"};
+		String[] values = {"A ","2 ","3 ","4 ","5 ","6 ","7 ",
+				"8 ","9 ","10","J ","Q ","K "};
 		for (int[] player : players) {
+			Arrays.sort(player);
 			for (int card : player) {
-				System.out.print(card +" ");
+				System.out.print(suits[card/13] + values[card%13]  +" ");
 			}
 			System.out.println();
 		}
