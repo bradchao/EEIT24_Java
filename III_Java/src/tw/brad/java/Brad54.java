c6b1c0ef4254e5eb3c7c27f83863689ee1d888f6
USER <USER@L04.r1804.tciii.org.tw>
2021-03-10 16:11:01 +0800

    03101610

--- III_Java/src/tw/brad/java/Brad54.java
+++ III_Java/src/tw/brad/java/Brad54.java
@@ -1,5 +1,8 @@
 package tw.brad.java;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,8 +13,24 @@ public class Brad54 {
 		try {
 			ServerSocket server = new ServerSocket(7777);
 			Socket socket = server.accept();
+			//-----------------------
+			InetAddress urip = socket.getInetAddress();
+			BufferedReader reader = 
+				new BufferedReader(
+					new InputStreamReader(
+						socket.getInputStream()));
+			String line; StringBuffer sb = new StringBuffer();
+			while ( (line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			reader.close();
+			//-----------------------
 			server.close();
-			System.out.println("TCP Receive OK");
+			
+			System.out.println(urip + ":" + sb);
+			
+			
+			
 		} catch (Exception e) {
 			System.out.println(e);
 		}
--- III_Java/src/tw/brad/java/Brad55.java
+++ III_Java/src/tw/brad/java/Brad55.java
@@ -1,6 +1,8 @@
 package tw.brad.java;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -11,6 +13,14 @@ public class Brad55 {
 		try {
 			Socket socket = 
 				new Socket(InetAddress.getByName("10.0.103.94"), 7777);
+			//---------------------------------------
+			BufferedWriter writer = 
+				new BufferedWriter(
+					new OutputStreamWriter(socket.getOutputStream()));
+			writer.write("Hello, III");
+			writer.flush();
+			writer.close();
+			//---------------------------------------------------
 			socket.close();
 			System.out.println("TCP Send OK");
 		} catch (Exception e) {
